41c7632ca4f45ba1e204d6496470d50647d85b61
NoMercy182 <shikaridiazz@gmail.com>
2022-03-07 22:42:14 +0300

    feat: add first task

---
+++ Test/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Типы данных
+
+// console.log(4/0); // получаем infinity
+// console.log('string' * 9); // получаем NaN
+
+// let und;
+// console.log(und); // получаем undefind
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.png', 6, {}, []];
+// console.log(arr[1]); // достаем второй элемент массива
+
